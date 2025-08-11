a385df8845527f0c06e09bb9a6c95b9dee780a87
devmosis <ryzond6728@gmail.com>
2025-08-12 03:13:19 +0900

    Revert "o"
    This reverts commit f173006423477420384eb91221ed04eecf35c94d.

--- packages/stores/src/account/base.ts
+++ packages/stores/src/account/base.ts
@@ -229,13 +229,13 @@ export class AccountStore<Injects extends Record<string, any>[] = []> {
 
   private _createWalletManager(wallets: MainWalletBase[]) {
     this._walletManager = new WalletManager(
-      this.chains as any,
+      this.chains,
       wallets,
       logger,
       true,
       true,
       ["https://daodao.zone", "https://dao.daodao.zone"],
-      this.walletManagerAssets as any,
+      this.walletManagerAssets,
       "icns",
       this.options.walletConnectOptions,
       undefined,
